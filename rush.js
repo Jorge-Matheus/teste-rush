a7b7d2f935906f54a95930f360cc03d0afed2ee9
Jorge-Matheus <126062144+Jorge-Matheus@users.noreply.github.com>
2023-04-17 12:48:33 -0300

    site-teste

---
+++ rush.js
@@ -0,0 +1,10 @@
+function abrirnav() {
+    document.getElementById("menuoculto").style.width="250px"
+    document.getElementById("principal").style.marginLeft="250px"
+}
+
+
+function fecharnav() {
+    document.getElementById("menuoculto").style.width="0"
+    document.getElementById("principal").style.marginLeft="0"
+}
